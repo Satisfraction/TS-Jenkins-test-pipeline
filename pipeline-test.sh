26e3d92f7c2af63c4fa03cb7da77202ce115a86a
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-09-22 20:15:10 +0200

    Add files via upload

---
+++ pipeline-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Start of the poem
+echo "Jenkins ist ein Werkzeug, das uns hilft, zu bauen und zu testen"
+echo "Es automatisiert die Pipeline, damit wir uns nicht stressen"
+echo "Es bietet viele Features, die uns das Leben leichter machen"
+echo "Wie Parameter, Post-Aktionen und Shared Libraries zum Sichern"
+echo "Aber Jenkins hat auch seine Tücken, die uns manchmal ärgern"
+echo "Wie Konfiguration, Skalierung und Sicherheit, die uns Sorgen bergen"
+echo "Es braucht viel Zeit und Mühe, um Jenkins zu beherrschen"
+echo "Und manchmal fragen wir uns, ob es sich wirklich lohnt zu investieren"
+echo "Doch am Ende des Tages, wenn wir sehen, was Jenkins kann"
+echo "Dann sind wir froh und dankbar, dass wir es haben"
+# End of the poem
+
+# Verion Nr
+echo "Version 1.0"
